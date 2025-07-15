2577a29f10b1e52d662cec1836e9a82bab81942a
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-07-15 17:26:07 +0700

    lesson2 done

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,10 +1,7 @@
-from tests.conftest import second_user
-
-
 def test_user_init(first_user, second_user):
     assert first_user.username == "user"
     assert first_user.email == "user@mail.ru"
-    assert len(first_user.tasks_list) == 2
+    assert len(first_user.task_list) == 2
 
     assert first_user.users_count == 2
     assert second_user.users_count == 2
